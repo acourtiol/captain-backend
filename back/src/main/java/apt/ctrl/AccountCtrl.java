46ea9ddabd827f54530b4449e08391e2787803e5
Jérémie Drouet <jeremie.drouet@tecumseh.com>
2014-05-15 14:09:30 +0200

    Add api response status annotations

--- back/src/main/java/apt/ctrl/AccountCtrl.java
+++ back/src/main/java/apt/ctrl/AccountCtrl.java
@@ -3,10 +3,8 @@ package apt.ctrl;
 import apt.model.Account;
 import apt.srv.AccountSrv;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author jeremie.drouet
@@ -27,11 +25,13 @@ public class AccountCtrl {
         this.accountSrv = accountSrv;
     }
 
+    @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
     @RequestMapping(value = "/get_by_id/{id_account}", method = RequestMethod.GET, headers = "Accept=application/json")
     public Account getById(@PathVariable(value = "id_account") Long idAccount) {
         return null; // TODO
     }
 
+    @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
     @RequestMapping(value = "/get_by_mail_address/{mail_address}", method = RequestMethod.GET, headers = "Accept=application/json")
     public Account getByMailAddress(@PathVariable(value = "mail_address") String mailAddress) {
         return null; // TODO
--- back/src/main/java/apt/ctrl/ProjectCtrl.java
+++ back/src/main/java/apt/ctrl/ProjectCtrl.java
@@ -3,10 +3,8 @@ package apt.ctrl;
 import apt.model.Project;
 import apt.srv.ProjectSrv;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,11 +28,13 @@ public class ProjectCtrl {
         this.projectSrv = projectSrv;
     }
 
+    @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
     @RequestMapping(value = "/get_by_owner/{id_account}", method = RequestMethod.GET, headers = "Accept=application/json")
     public List<Project> getByOwner(@PathVariable(value = "id_account") Long idAccount) {
         return new ArrayList<Project>(); // TODO
     }
 
+    @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
     @RequestMapping(value = "/get_by_owner_and_name/{id_account}/{name}", method = RequestMethod.GET, headers = "Accept=application/json")
     public Project getByOwnerAndName(@PathVariable(value = "id_account") Long idAccount, @PathVariable(value = "name") String name) {
         return null; // TODO
