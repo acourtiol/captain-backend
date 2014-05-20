c3cab482e0341acc1726b0363b6f3b3490a902fd
Jérémie Drouet <jeremie.drouet@gmail.com>
2014-05-20 21:26:19 +0200

    Jetty integration to maven

---
+++ src/main/java/apt/Application.java
@@ -0,0 +1,20 @@
+package apt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Jérémie Drouet on 20/05/14.
+ */
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
