eadd85026b034ab0039b2a15012dcedb0bd34131
Societe GENERALE <societe-generale-32@e2r1p3.42.fr>
2014-05-24 20:19:24 +0200

    Add project rest services

--- src/main/java/apt/dao/GeneralDao.java
+++ src/main/java/apt/dao/GeneralDao.java
@@ -4,7 +4,7 @@ package apt.dao;
 import java.util.List;
 
 public interface GeneralDao<T> {
-    public T findOne(String param, String value);
+    public T findOne(String param, Object value);
     public T save(T o);
     public void delete(T o);
     public List<T> getList(String param, Object value);
