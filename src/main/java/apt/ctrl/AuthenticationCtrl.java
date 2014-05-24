a7086700768c6347f3bf4d8888012ff309ccdd29
yann91@gmail.com <societe-generale-32@e2r1p3.42.fr>
2014-05-24 20:58:13 +0200

    Remove unused imports

--- src/main/java/apt/ctrl/AuthenticationCtrl.java
+++ src/main/java/apt/ctrl/AuthenticationCtrl.java
@@ -1,6 +1,5 @@
 package apt.ctrl;
 
-import apt.model.Authentication;
 import apt.srv.AuthenticationSrv;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
