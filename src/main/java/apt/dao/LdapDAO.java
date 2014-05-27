f211d94b4846c081005551312800b2ac65073339
Jérémie Drouet <jeremie.drouet@gmail.com>
2014-05-27 22:11:55 +0200

    Add spring ldap configuration

---
+++ src/main/java/apt/dao/LdapDAO.java
@@ -0,0 +1,12 @@
+package apt.dao;
+
+import java.util.List;
+
+/**
+ * Created by Jérémie Drouet on 27/05/14.
+ */
+public interface LdapDAO {
+
+    // TODO http://projects.spring.io/spring-ldap/#quick-start
+
+}
